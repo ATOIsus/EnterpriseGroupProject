f462c6c79987044f812a81f6ea17ef708d8ff170
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-15 14:28:27 +0545

    Libraries downloaded.

--- Arduino/ArduinoCode/ArduinoCode.ino
+++ Arduino/ArduinoCode/ArduinoCode.ino
@@ -1,5 +1,15 @@
 //Arduino Code.
 
+
+#include <SoftwareSerial.h>
+#include <Wire.h>                              // To communicate with I2C.
+#include <LiquidCrystal_I2C.h>                 // To control I2C(LCD) displays with functions.
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);            // Set the LCD address to 0x27 for a 16 chars and 2 line display.
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
