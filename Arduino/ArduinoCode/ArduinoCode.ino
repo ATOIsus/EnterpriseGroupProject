0f4171fe7fb9ce94f326c164fdfbd57186a85fe2
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-15 14:44:22 +0545

    Variables added for sensors.

--- Arduino/ArduinoCode/ArduinoCode.ino
+++ Arduino/ArduinoCode/ArduinoCode.ino
@@ -7,21 +7,47 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);            // Set the LCD address to 0x27 for a 16 chars and 2 line display.
 
+//Sensors
+int tempAquarium = A0;
 
+int tempHotWater = A1;
+
+int trigLevelAquarium = D2;
+int echoLevelAquarium = D3;
+
+int trigLevelFood = D5;
+int echoLevelFood = D6;
 
 
 void setup() {
+
+  //Declaring Pins.
+  pinMode(A0, INPUT);
+  
+  pinMode(A1, INPUT);
+
+  pinMode(D2, OUTPUT);
+  pinMode(D3, INPUT);
+
+  pinMode(D4, OUTPUT);
+  pinMode(D5, INPUT);
+
+  
+  //Seeial Monitor.
   Serial.begin(9600);
 
+  //LCD.
   lcd.init();                              // Initialize the LCD.
   lcd.backlight();                         // For the backlight of the LCD.
+ 
   lcd.setCursor(0, 0);                                // To print to the LCD.  setCursor(column,row).
   lcd.print("Welcome ");
+ 
   lcd.setCursor(7, 1);                                // To print to the LCD.  setCursor(column,row).
   lcd.print("User!");
+  
   delay(3000);                                        // Delay the program by 3 seconds. i.e. Welcome User! is displayed for 3 seconds.
   lcd.clear();                                        // To clear the LCD.
-
 }
 
 void loop() {
