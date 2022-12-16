60a929d6e518efbf5acfeb6bc438f4adda28a464
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-16 10:20:25 +0545

    Serial communication between Arduino and NodeMCU started.

--- Arduino/NodeMCUCode/NodeMCUCode.ino
+++ Arduino/NodeMCUCode/NodeMCUCode.ino
@@ -1,6 +1,13 @@
 //Node MCU Code.
 
 
+//Serial Communication with Arduino.
+bool startCount = false;
+int stringLength = 74;
+int count = 0;
+String wholeString;
+
+
 
 void setup() {
    Serial.begin(9600);
