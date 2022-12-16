fb2811cdeab4b636954c8a7f0213b26d0179a282
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-16 17:54:01 +0545

    Variables assigned for the InputFields.

--- Unity/Code/unityCode.cs
+++ Unity/Code/unityCode.cs
@@ -18,3 +18,16 @@ public class click : MonoBehaviour
 
     public VirtualButtonBehaviour Vb_on;
  
+
+  void Start()
+    {
+        tempAquarium = GameObject.Find("TextInputField").GetComponent<InputField>();
+        tempHotWater = GameObject.Find("TextInputField1").GetComponent<InputField>();
+        levelAquarium = GameObject.Find("TextInputField2").GetComponent<InputField>();
+        levelFood = GameObject.Find("TextInputField3").GetComponent<InputField>();
+        
+
+        Vb_on.RegisterOnButtonPressed(OnButtonPressed_on);
+    }
+
+
