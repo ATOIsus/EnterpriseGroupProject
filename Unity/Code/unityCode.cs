61927214be11fff2b9fd60a2f7f4a5671d8ddbee
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-16 17:45:59 +0545

    Variables declared for InputFields and VirtualButton.

--- Unity/Code/unityCode.cs
+++ Unity/Code/unityCode.cs
@@ -6,3 +6,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using Vuforia;
+
+
+
+public class click : MonoBehaviour
+{
+    InputField tempAquarium;
+    InputField tempHotWater;
+    InputField levelAquarium;
+    InputField levelFood;
+
+    public VirtualButtonBehaviour Vb_on;
+ 
