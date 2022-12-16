5d46374f1df4a843028da274b3c16a3b9994e5f8
ATOIsus <sabinmaharjan884@gmail.com>
2022-12-16 10:24:38 +0545

    Function for serial communication done.

--- Arduino/NodeMCUCode/NodeMCUCode.ino
+++ Arduino/NodeMCUCode/NodeMCUCode.ino
@@ -8,6 +8,13 @@ int count = 0;
 String wholeString;
 
 
+//Sensor data.
+String tempAquarium;
+String tempHotWater;
+String levelAquarium;
+String levelFood;
+
+
 
 void setup() {
    Serial.begin(9600);
@@ -20,4 +27,40 @@ void setup() {
 void loop() {
  
 
+}
+
+
+
+
+
+
+void serialCommunication() {
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '$') {
+      startCount = true;
+    }
+
+    if (startCount == true )
+    {
+
+      if (count < stringLength)
+      {
+        wholeString = String(wholeString + c);
+        count++;
+      }
+      if (count >= stringLength) {
+        count = 0;
+        startCount = false;
+
+        tempAquarium = wholeString.substring(2, 5);
+        tempHotWater = wholeString.substring(7, 9);
+        levelAquarium = wholeString.substring(12, 15);
+        levelFood = wholeString.substring(17, 19);
+
+        wholeString = "";
+      }
+    }
+  }
 }
